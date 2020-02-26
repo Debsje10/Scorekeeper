996bf80a4d26841974d62e11b093a1dd19bbcb3c
debsje10 <deborah.vd.rhee@gmail.com>
2020-02-26 20:23:54 +0100

    added boolean and if statments for player 1/2 button

--- Score.js
+++ Score.js
@@ -6,18 +6,35 @@ var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#disPlay");
 var p1Display1 = document.querySelector("#disPlay1");
 var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
-console.log(p1Display)
+
+
+console.log(p1Display1);
 
 p1Button.addEventListener("click", function(){
-    p1Score++;
-    p1Display.textContent = p1Score;
+    if (!gameOver){
+        p1Score++;
+        if(p1Score===winningScore){
+            alert("GAME OVER!!");
+        }
+        p1Display.textContent = p1Score;
+    }
 }); 
 
 
 p2Button.addEventListener("click", function(){
-    p1Score++;
-    p1Display1.textContent = p1Score;
+    if(!gameOver){
+        p2Score++;
+        if(p2Score===winningScore){
+            alert("GAME OVER!!");
+        }
+        
+    }
+    
+    p1Display1.textContent = p2Score;
 
 }); 
 
